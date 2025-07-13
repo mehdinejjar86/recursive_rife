241e1589c898663e132a4dd20b05303d32a812f6
mehdinejjar86 <mehdinejjar@hotmail.com>
2025-07-13 22:38:12 +0100

    small naming corrections

--- inference_img.py
+++ inference_img.py
@@ -70,7 +70,7 @@ def main():
   warnings.filterwarnings("ignore")
 
   parser = argparse.ArgumentParser(description='Interpolation for a pair of images')
-  parser.add_argument('--model', dest='modelDir', type=str, default='rifev4.25', help='directory with trained model files')
+  parser.add_argument('--model', dest='modelDir', type=str, default='rifev4_25', help='directory with trained model files')
   parser.add_argument('--UHD', dest='UHD', action='store_true', help='support 4k images')
   parser.add_argument('--scale', dest='scale', type=float, default=1.0, help='Try scale=0.5 for 4k')
   parser.add_argument('--multi', dest='multi', type=int, default=2)
